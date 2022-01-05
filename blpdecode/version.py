eba99443eec5bfbd33b034a6ae263d3517f6471e
Matt Krueger <mkrueger@rstms.net>
2022-01-05 10:57:30 -0600

    Bump version: 0.4.3 → 0.4.4

--- blpdecode/version.py
+++ blpdecode/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-01-05 10:55:37-06:00"
+__timestamp__ = "2022-01-05 10:57:30-06:00"
